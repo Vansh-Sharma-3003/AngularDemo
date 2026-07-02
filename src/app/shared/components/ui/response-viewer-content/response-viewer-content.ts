c1ccc74c613775950972531b854236832f8e9a3f
Vansh-Sharma-3003 <vs266548@gmail.com>
2026-07-02 20:42:40 +0530

    add more component for response viewer

--- src/app/shared/components/ui/response-viewer-content/response-viewer-content.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-response-viewer-content',
-  imports: [],
-  templateUrl: './response-viewer-content.html',
-  styleUrl: './response-viewer-content.css',
-})
-export class ResponseViewerContent {}
