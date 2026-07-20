5876297e89f49111ce29291b58571a7ac3c49217
Vansh-Sharma-3003 <vs266548@gmail.com>
2026-07-20 19:48:49 +0530

    working on shared grid component

--- src/app/shared/components/ui/grid/grid.ts
+++ src/app/shared/components/ui/grid/grid.ts
@@ -1,8 +1,8 @@
 
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { GridColumn, MOCK_TABLE_DATA } from '../../../../model/ui/table-data';
+import { GridColumn } from '../../../../model/ui/table-data';
 @Component({
   selector: 'app-grid',
   imports: [MatTableModule, MatSortModule],
@@ -11,32 +11,28 @@ import { GridColumn, MOCK_TABLE_DATA } from '../../../../model/ui/table-data';
 })
 export class Grid<T> {
 @Input() data: T[] = [];
-
   @Input() columns: GridColumn<T>[] = [];
+  @Output() cellClick = new EventEmitter<T>();
 
-
-  @Output()
-  cellClick = new EventEmitter<T>();
-
+  @ViewChild(MatSort) sort!: MatSort;
 
   dataSource = new MatTableDataSource<T>();
-
   displayedColumns: string[] = [];
 
-
   ngOnChanges() {
-
     this.dataSource.data = this.data;
+    this.displayedColumns = this.columns.map(x => x.columnDef as string);
 
-    this.displayedColumns =
-      this.columns.map(x => x.columnDef as string);
-
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+    }
   }
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
-  onClick(row:T){
-
+  onClick(row: T) {
     this.cellClick.emit(row);
-
   }
 }
